62ea825fe2e59c5d73b37869868348e17f1caa4d
Dominik Heeb <d1heeb@hsr.ch>
2015-02-24 13:00:28 +0100

    Initial Commit

---
+++ SNMPMonitorSolution/SNMPManager.PresentationLayer.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SNMPManager.PresentationLayer.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
