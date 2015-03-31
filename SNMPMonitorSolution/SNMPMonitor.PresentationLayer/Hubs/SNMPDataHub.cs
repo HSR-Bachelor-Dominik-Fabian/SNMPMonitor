7595d0feddc2197e859abd17b68fd88d25ade7e4
Dominik Heeb <d1heeb@hsr.ch>
2015-03-31 09:27:47 +0200

    SNMP-61 #time 1h 30m SignalR Kommunikation Server zu Client erstellt

--- SNMPMonitorSolution/SNMPMonitor.PresentationLayer/Hubs/SNMPDataHub.cs
+++ SNMPMonitorSolution/SNMPMonitor.PresentationLayer/Hubs/SNMPDataHub.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace SNMPMonitor.PresentationLayer.Hubs
 {
+    [HubName("snmpDataHub")]
     public class SNMPDataHub : Hub
     {
-        public static void Show()
+        public void SendSNMPData()
         {
             IHubContext context = GlobalHost.ConnectionManager.GetHubContext<SNMPDataHub>();
-            context.Clients.All.displayStatus();
+            Random rnd = new Random();
+            context.Clients.Group(".1.3.2.14.23").receiveData(rnd.Next(100));
+        }
+
+        public Task JoinDataGroup(string groupName)
+        {
+            return Groups.Add(Context.ConnectionId, groupName);
+        }
+
+        public Task LeaveDataGroup(string groupName)
+        {
+            return Groups.Remove(Context.ConnectionId, groupName);
         }
     }
 }
