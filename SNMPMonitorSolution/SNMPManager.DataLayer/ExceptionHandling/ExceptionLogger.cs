3349e203a8cae55269ea7f3baf31cc0457ae0863
Dominik Heeb <d1heeb@hsr.ch>
2015-04-20 14:03:48 +0200

    SNMP-23 Grundlegende Struktur ExceptionHandling erstellt

---
+++ SNMPMonitorSolution/SNMPManager.DataLayer/ExceptionHandling/ExceptionLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNMPManager.DataLayer.ExceptionHandling
+{
+    public class ExceptionLogger
+    {
+        public static void LogException(string category, Exception exc)
+        {
+
+        }
+
+        public static void SaveExceptionToDB(string category, Exception exc)
+        {
+
+        }
+    }
+}
