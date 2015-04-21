31661c72ba2c63a290ee366a04314a1b63b4c5f8
fabiankeller <fabian.keller91@gmail.com>
2015-04-21 17:48:33 +0200

    SNMP-30 BUGFIX: problems with the getMonitoringTypesForAgent stored procedure. The UI didn't get the data

--- SNMPMonitorSolution/SNMPMonitor.DataLayer/DataModels/MonitoringTypeDataModel.cs
+++ SNMPMonitorSolution/SNMPMonitor.DataLayer/DataModels/MonitoringTypeDataModel.cs
@@ -11,14 +11,12 @@ namespace SNMPMonitor.DataLayer
         private readonly int _monitoringTypeNr;
         private readonly String _description;
         private readonly String _objectID;
-        private readonly bool _isLongTimeCheck;
 
-        public MonitoringTypeDataModel(int monitoringTypeNr, String description, String objectID, bool IsLongTimeCheck)
+        public MonitoringTypeDataModel(int monitoringTypeNr, String description, String objectID)
         {
             _monitoringTypeNr = monitoringTypeNr;
             _description = description;
             _objectID = objectID;
-            _isLongTimeCheck = IsLongTimeCheck;
         }
 
         public int MonitoringTypeNr
@@ -42,14 +40,6 @@ namespace SNMPMonitor.DataLayer
             get
             {
                 return _objectID;
-            }
-        }
-
-        public bool IsLongTimeCheck
-        {
-            get
-            {
-                return _isLongTimeCheck;
             }
         }
     }
